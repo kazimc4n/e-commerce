f4a42060093e80a9d302d37b15bbb7d033492ff1
Kazım CAN <kcan21@ku.edu.tr>
2025-07-29 10:48:04 +0300

    Add placeholder routes for orders, reviews, users and admin

---
+++ server/routes/reviews.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Review routes will be implemented here
+// For now, just export empty router to prevent server errors
+
+module.exports = router; 
